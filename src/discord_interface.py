d4dbf648c1636e89e6f5427ec09c6fa1e347c15a
kevincal1226 <kevincal@umich.edu>
2024-08-28 11:47:34 -0400

    Helpers | added integration test helper functions

--- src/discord_interface.py
+++ src/discord_interface.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 BOT_COMAMND_PREFIX = "!"
-
+RETRY_LIMIT_AMOUNT = 10
 
 # Set up client and intents
 intents = discord.Intents.default()
@@ -11,7 +12,53 @@ intents.members = True
 client = commands.Bot(intents=intents, command_prefix=BOT_COMAMND_PREFIX)
 
 
-async def send_message(
-    msg: str, channel: discord.channel.TextChannel | discord.channel.DMChannel
-) -> None:
+async def send_message(msg: str, channel: discord.channel.TextChannel | discord.channel.DMChannel) -> None:
     await channel.send(msg)
+
+
+async def wait_for_new_message_in_same_channel(sent_message):
+    retry_limit = RETRY_LIMIT_AMOUNT
+    while retry_limit > 0:
+        async for message in sent_message.channel.history(limit=1):
+            if message.id != sent_message.id:
+                return message
+            break
+        if retry_limit > 0:
+            retry_limit -= 1
+            await asyncio.sleep(0.5)
+    raise Exception("Expected message was never sent")
+
+
+async def wait_for_new_message_in_DM(last_message_id: int, discord_id: int, reaction_count: int = 0):
+    retry_limit = RETRY_LIMIT_AMOUNT
+    while retry_limit > 0:
+        discord_user = await client.fetch_user(discord_id)
+        channel = await discord_user.create_dm()
+        async for message in channel.history(limit=1):
+            if message.id != last_message_id and len(message.reactions) >= reaction_count:
+                return message
+            break
+        if retry_limit > 0:
+            retry_limit -= 1
+            await asyncio.sleep(0.5)
+    raise Exception("Expected message in DM was never sent")
+
+
+async def get_last_message_for_channel(channel_id) -> discord.Message | None:
+    channel = client.get_channel(channel_id)
+
+    if channel is None:
+        channel = await client.fetch_channel(channel_id)
+
+    async for message in channel.history(limit=1):
+        return message
+
+    return None
+
+
+async def get_last_message_for_user(discord_id: int) -> discord.Mesage | None:
+    discord_user = await client.fetch_user(discord_id)
+    channel = await discord_user.create_dm()
+
+    async for message in channel.history(limit=1):
+        return message
